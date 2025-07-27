5e6e482a154704b4f7a66c9274b0f514e021f9f9
Kalpit Malviya <48874264+kalpitmalviya@users.noreply.github.com>
2025-07-27 23:29:23 +0530

    Add files via upload

---
+++ EKS/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+
+}
+
+variable "public_subnets" {
+  description = "Public Subnets"
+  type        = list(string)
+
+}
+
+variable "private_subnets" {
+  description = "Private Subnets"
+  type        = list(string)
+
+}
